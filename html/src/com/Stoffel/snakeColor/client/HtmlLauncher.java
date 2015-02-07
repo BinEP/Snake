dacc91630e8f6f2dfc5db20ea11d6980febed24e
BinEP <bradystoffel@gmail.com>
2015-02-07 00:32:52 -0600

    First commit

---
+++ html/src/com/Stoffel/snakeColor/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.Stoffel.snakeColor.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.Stoffel.snakeColor.Snake;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Snake();
+        }
+}
